eb52d8d84b90bd3c1a34edeb4e2dcb291fe0f943
anton <andong69@outlook.com>
2023-07-12 00:11:16 +0800

    private variable

--- new/new/test.cpp
+++ new/new/test.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[])
+class Student{
+private:
+	char *name;
+public:
+	Student(){
+		cout << "name = NULL" << endl;
+	}
+	void set(char *string){
+		name = string;
+	}
+	void get(){
+		cout << "name = " << name << endl;
+	}
+	~Student(){
+		cout << "name = NULL" << endl;
+	}
+};
+
+int main()
 {
 	cout << "hello new_test!" << endl;
+	Student *ming = new Student;
+
+	char *string = "anton";
+	ming->Student::set(string);
+	ming->Student::get();
+
+	delete ming;
 	while(1);
 
 	return 0;
